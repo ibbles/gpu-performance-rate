b0cbb9cc4c526d5cc5428c7bc74f6a41841ad51f
Martin Nilsson <ibbles@hotmail.com>
2021-01-12 22:11:37 +0100

    Replace multiple colors with gamma corrected red/green gradient

--- settings.py
+++ settings.py
@@ -1,14 +1,6 @@
+from math import isnan
 from types import SimpleNamespace
 from typing import List
-
-
-def pick_color_interval(value: float, stops: List[SimpleNamespace]):
-    for lower, upper in zip(stops[:-1], stops[1:]):
-        if value >= lower.value and value < upper.value:
-            return SimpleNamespace(lower=lower, upper=upper)
-
-    invalid = SimpleNamespace(value=value, color=[110, 110, 110])
-    return SimpleNamespace(lower=invalid, upper=invalid)
 
 
 def mix(f, a, b):
@@ -19,35 +11,19 @@ def mix_colors(f, a, b):
     return [mix(f, c1, c2) for c1, c2 in zip(a, b)]
 
 
-def pick_color(value: float, colors: List[List[int]]):
-    interval = pick_color_interval(value, colors)
-    width = interval.upper.value - interval.lower.value
-    if width > 0.0:
-        progress = (value - interval.lower.value) / width
-    else:
-        progress = 0.0
-    return mix_colors(progress, interval.lower.color, interval.upper.color)
+def pick_color(value: float, color: List[int]):
+    progress = min(value, 100) / 100.0
+    f = progress**0.4
+    return mix_colors(f, [0.0, 0.0, 0.0], color)
 
 
 def color_range(value: float):
-    positive_stops = [
-        SimpleNamespace(value=0, color=[0, 0, 0]),
-        SimpleNamespace(value=20, color=[0, 125, 8]),
-        SimpleNamespace(value=100, color=[187, 91, 201]),
-        SimpleNamespace(value=300, color=[0, 255, 0])
-    ]
-
-    negative_stops = [
-        SimpleNamespace(value=0, color=[0, 0, 0]),
-        SimpleNamespace(value=20, color=[26, 185, 217]),
-        SimpleNamespace(value=100, color=[255, 111, 28]),
-        SimpleNamespace(value=300, color=[255, 0, 0])
-    ]
-
+    if isnan(value):
+        return [127] * 3
     if value >= 0:
-        return pick_color(value, positive_stops)
+        return pick_color(value, [0, 255, 0])
     else:
-        return pick_color(-value, negative_stops)
+        return pick_color(-value, [255, 0, 0])
 
 
 def make_settings():
